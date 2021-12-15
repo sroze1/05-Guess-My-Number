fd2e426d36c0620070b2ffc449ec01eede63097b
S Roze <sherozem4@gmail.com>
2021-12-15 14:31:37 +0000

    short example of function which detects prime number.

--- 05-Guess-My-Number/starter/Code/script.js
+++ 05-Guess-My-Number/starter/Code/script.js
@@ -1,2 +1,23 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Tatta!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+function divisibleCheck(integer1) {
+  let i = 1;
+
+  while (i < integer1) {
+      i++;
+    if (integer1 % i === 0) {
+      return `${integer1} is not a prime number mcgregoc am comin 4 your fkwn ass`
+    } else return `${integer1} is a prime`
+  }
+}
+
+
