31e71c11d6c1335b55626b3f5a87811beed4c86a
S Roze <sherozem4@gmail.com>
2021-12-20 23:54:28 +0000

    Guess the number game has been completed. This includes a high score feature, which saves throughout continued games. This also includes a button which resets the score and reloads the page allowing you to attempt to top your previous high score

--- 05-Guess-My-Number/starter/Code/script.js
+++ 05-Guess-My-Number/starter/Code/script.js
@@ -1,62 +1,82 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
 
-// document.querySelector('.message').textContent = 'Tatta!';
-
+// Variables set outside the functions
 console.log(document.querySelector('.guess').value);
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+let guess;
 
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-
-// let score = Number(document.querySelector('.score').textContent);
+let highscore = 0;
 let score = 20;
 
 document.querySelector('.number').textContent = '?';
 
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
   if (score > 1) {
-    //Invalid Number
+    // When invalid Number
     if (!guess) {
       document.querySelector('.message').textContent = 'No number!';
     }
-    // Correct guess
+    // When correct guess
     else if (guess === randomNumber) {
       document.querySelector('.message').textContent =
         'Correct number, you win!';
       document.querySelector('.number').textContent = randomNumber;
       document.querySelector('body').style.backgroundColor = 'green';
-      document.querySelector('.highscore').textContent = score;
+      document.querySelector('.number').style.width = '30rem';
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
     }
-    // Guess is higher than number
+    // When guess is higher than number
     else if (guess > randomNumber) {
       score--;
       document.querySelector('.message').textContent =
         'The hidden number is lower g';
       document.querySelector('.score').textContent = score;
     }
-    // Guess is lower than number
+    // When guess is lower than number
     else if (guess < randomNumber) {
       score--;
       document.querySelector('.message').textContent =
         'The hidden number is higher g';
       document.querySelector('.score').textContent = score;
     }
+
+    // When the score hits 0
   } else {
     document.querySelector('.message').textContent = 'Bounce G You lost!';
     document.querySelector('.score').textContent = 0;
   }
 });
 
-// function thisIsThisIsTheChange
-// function thisIsTheLing
-// function divisibleCheck(integer1) {
-//   let i = 1;
+///////////////////////////////////////////
+///////////////////////////////////////////
+// When the user clicks 'Again!' button..
+// Reset everything
+document.querySelector('.again').addEventListener('click', function () {
+  // Reset the message
+  document.querySelector('.message').textContent = 'Start guessing...';
 
-//   while (i < integer1) {
-//       i++;
-//     if (integer1 % i === 0) {
-//       return `${integer1} is not a prime number mcgregoc am comin 4 your fkwn ass`
-//     } else return `${integer1} is a prime`
-//   }
-// }
+  // Reset the random number
+  randomNumber = randomNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // Reset the box with the number:
+  document.querySelector('.number').textContent = '?';
+
+  // Reset the width of the number:
+  document.querySelector('.number').style.width = '15rem';
+
+
+  // Reset the score
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  // Reset the background color
+  document.querySelector('body').style.backgroundColor = '#222';
+});
